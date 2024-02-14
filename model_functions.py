396a6c1a2a858a09f53f82e5d14f01623020c7de
Natalia Wrześniak <natalia.wrzesniak19@gmail.com>
2024-02-14 11:46:24 +0100

    descriptions added

--- model_functions.py
+++ model_functions.py
@@ -9,18 +9,34 @@ import numpy as np
 
 
 def fitter(estimator, param_grid, X_train, y_train):
-    grid_model = GridSearchCV(estimator, param_grid)
+    """
+    Finding the best estimator based on chosen parameters via GridSearchCV
+
+    :param estimator: basic estimator chosen for GridSearchCV
+    :param param_grid: param_grid with params associated with chosen estimator
+    :param X_train: features to train on
+    :param y_train: labels to train on
+    :return:
+    """
+    grid_model = GridSearchCV(estimator, param_grid, scoring="roc_auc")
     grid_model.fit(X_train, y_train)
     grid_model = grid_model.best_estimator_
     return grid_model
 
 
 def evaluate(estimator, X_test, y_test):
+    """
+    evaluation the performance of chosen estimator
+    :param estimator: estimator chosen for evaluation
+    :param X_test:  features to evaluate model on
+    :param y_test: labels to evaluate model on
+    :return: ConfusionMatrixDisplay, classification report, roc_auc_score
+    """
     print(f"{estimator}")
     predictions = estimator.predict(X_test)
     cm = confusion_matrix(y_test, predictions)
     disp = ConfusionMatrixDisplay(confusion_matrix=cm)
-    disp.plot(cmap="plasma")
+    disp.plot()
     print(classification_report(y_test, predictions))
 
     def change(value):
